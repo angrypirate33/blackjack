d8cd9f4ea95cbbb6c6e943b5fca388b76d0ea30b
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-04-25 16:44:05 -0400

    Hides dealers first card value from the dealer score display (per blackjack rules).

--- js/main.js
+++ js/main.js
@@ -142,14 +142,14 @@ function dealFirstRound() {
     setTimeout(() => pTotal = shuffledDeck[0].value, 1001)
     setTimeout(() => dOne.className = `card ${shuffledDeck[1].face} back`, 2000)
     setTimeout(() => cardSound.play(), 2000)
-    setTimeout(() => dTotal = shuffledDeck[1].value, 2001)
+    // setTimeout(() => dTotal = shuffledDeck[1].value, 2001)
     setTimeout(() => pTwo.className = `card ${shuffledDeck[2].face}`, 3000)
     setTimeout(() => cardSound.play(), 3000)
     setTimeout(() => pTotal += shuffledDeck[2].value, 3001)
     setTimeout(renderPlayerScore, 3002)
     setTimeout(() => dTwo.className = `card ${shuffledDeck[3].face}`, 4000)
     setTimeout(() => cardSound.play(), 4000)
-    setTimeout(() => dTotal += shuffledDeck[3].value, 4001)
+    setTimeout(() => dTotal = shuffledDeck[3].value, 4001)
     setTimeout(renderDealerScore, 4002)
 }
 
